41e44eb51b387cf0302cec8fbd11c1d87ff1cbb5
tgcarter <tgcarter@live.co.uk>
2016-04-03 17:57:14 +0100

    Database

--- js/index.js
+++ js/index.js
@@ -1,30 +1,58 @@
 function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("deviceready", onDeviceReady, false);
 }
 
-function onDeviceReady() {
+function onDeviceReady(){
     navigator.splashscreen.hide();
+    var db = window.openDatabase("quickquiz", 1.0, "Questions DB", 1024 * 1024);
+	db.transaction(createTable, error, addsuccess);
+	Questions();
 }
-
-/*//Database Creation
-var db = window.openDatabase("quickquiz", 1.0, "Questions DB", 1024 * 1024);
-db.transaction(createTable, error, success);
-	
+var timeleft = true;
+var questions = 0;
+$(document).on("pageshow","#game",function(){
+	var count=5;
+	document.getElementById("time").innerHTML=count;
+	var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+	function timer() {
+	  	count=count-1;
+	  	if (count <= 0){
+	    	clearInterval(counter);
+	    	document.getElementById("time").innerHTML="0";
+	    	$("#quiz").html("<h1 class='center-wrapper'>Times Up!</h1>");
+	    	timeleft = false;
+	    	return;
+	  	} else {
+	  		document.getElementById("time").innerHTML=count; // watch for spelling
+	  	}
+	 	
+	}
+	$(document).on("pagebeforehide","#game",function(){
+		clearInterval(counter);
+	});
+});
 function createTable(tx) {	
- 	tx.executeSql("CREATE TABLE IF NOT EXISTS questions(id UNIQUE, question TEXT, answer INT)");
+ 	tx.executeSql("CREATE TABLE IF NOT EXISTS questions(_id INTEGER PRIMARY KEY, question VARCHAR, answer INT)", addsuccess, adderror);
+}//Add Questions
+function addsuccess() {
+	db.transaction(addQuestions, error);
 }
-	
-function success() {
-	db.transaction(addFood, error);
-}
-	
-function addFood(tx) {
-	tx.executeSql("INSERT INTO food(id, title) VALUES (1, 'chips')");
-	tx.executeSql("INSERT INTO food(id, title) VALUES (2, 'cake')");
-	tx.executeSql("INSERT INTO food(id, title) VALUES (3, 'nachos')");
-}
-	
 function error(err) {
 	alert("An Error has occured:" + err.message);
-}*/
-
+}
+function addQuestions(tx) {
+	tx.executeSql("INSERT INTO questions(question, answer) VALUES ('Is New York the capital of America?', 0)");
+	tx.executeSql("INSERT INTO questions(question, answer) VALUES ('The Great Wall Of China is visible from the moon?', 0)");
+	tx.executeSql("INSERT INTO questions(question, answer) VALUES ('The ‘black box’ in an aeroplane is black?', 0)");
+	tx.executeSql("INSERT INTO questions(question, answer) VALUES ('The record for the longest rail tunnel is held by the Channel Tunnel between Britain and France?', 0)");
+}
+//Get Questions
+function Questions (){
+	function getQuestions(tx){
+		tx.executeSql('SELECT * FROM questions', [], getsuccess, error);
+	}
+	function getsuccess(tx) {
+		db.transaction(addQuestions, error);
+		questions = results.rows.length;
+	}
+}
