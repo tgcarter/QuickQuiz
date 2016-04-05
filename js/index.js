af5529ca0d3598b507014800301c077ca004d341
tgcarter <tgcarter@live.co.uk>
2016-04-05 12:41:19 +0100

    Updates

--- js/index.js
+++ js/index.js
@@ -1,31 +1,39 @@
+var isOffline;
+var timeleft = true;
+var qnum = null;
+var answercorrect = 0;
+var count;
+var useshake = true;
 function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);    
+    document.addEventListener("deviceready", onDeviceReady, false);
+    isOffline = 'onLine' in navigator && !navigator.onLine; 
+    if ( isOffline ) {
+	}
+	else {
+	}
 }
 function onDeviceReady(){
     navigator.splashscreen.hide();
 }
-var timeleft = true;
-var qnum = null;
-var answercorrect = 0;
-var questions = $.getJSON( "questions.json");
-    	/*{"question":"Is New York the capital of America?", "answer":"0"},
-    	{"question":"The Great Wall Of China is visible from the moon?", "answer":"0"},
-    	{"question":"The ‘black box’ in an aeroplane is black?", "answer":"0"},
-    	{"question":"The record for the longest rail tunnel is held by the Channel Tunnel between Britain and France?", "answer":"0"},
-	];*/
-$(document).on("pageshow","#game",function(){
-	shake.startWatch(onShake, 100);
-	var count=60;
+$(document).on("pagebeforeshow","#game",function(){
+	$("#timeup").hide();
+	$("#quiz").show();
+	count=5;
 	timeleft = true;
 	answercorrect = 0;
 	document.getElementById("time").innerHTML=count;
+});
+$(document).on("pageshow","#game",function(){ 	
+	//shake.startWatch(onShake, 100);
 	var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 	function timer() {
 	  	count--;
 	  	if (count <= 0){
 	    	clearInterval(counter);
 	    	document.getElementById("time").innerHTML="0";
-	    	$("#quiz").html("<h1 class='center-wrapper'>Times Up!</h1><h2 class='center-wrapper'>Your score was "+answercorrect+"!</h2>");
+	    	$("#quiz").hide();
+	    	document.getElementById("score").innerHTML=answercorrect;
+	    	$("#timeup").show();
 	    	timeleft = false;
 	    	return;
 	  	} else {
@@ -36,28 +44,31 @@ $(document).on("pageshow","#game",function(){
 	$("#true").on("tap",function(){
 		if (questions[qnum].answer=="1"){
 			answercorrect++;
-			console.log(answercorrect);
 		}
 		QuestionDisplay();
 	});
 	$(document).on("pagebeforehide","#game",function(){
 		clearInterval(counter);
-		shake.stopWatch();
+		//shake.stopWatch();
 	});
-
 });
 function QuestionDisplay(){
 	if (timeleft===true){
-	 	qnum = Math.floor(Math.random() * Object.keys(questions).length) + 0;
+	 	qnum = Math.floor(Math.random() * questions.length) + 0;
 	 	var showquestion = questions[qnum].question;
 	 	document.getElementById("question").innerHTML=showquestion;
-	 	console.log(questions[qnum].answer);
 	}
 }
-var onShake = function () {
-	if (questions[qnum].answer=="0"){
-		answercorrect++;
-		console.log(answercorrect);
-	}
-	QuestionDisplay();
+if (onShake==true){
+	var onShake = function () {
+		if (questions[qnum].answer=="0"){
+			answercorrect++;
+		}
+		QuestionDisplay();
+	};
 };
+$(document).on("pageshow","#settings",function(){
+	$("#toggleshake").on("tap",function(){
+		useshake = !useshake;
+	});
+});
